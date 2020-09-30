59e82c538924a960fc55445c0ad7dea9c2f54b84
kgober <kgober@gmail.com>
2020-09-30 14:40:53 -0400

    support multiple files on one BASIC tape

--- Tape Dump/Program.cs
+++ Tape Dump/Program.cs
@@ -134,6 +134,11 @@ namespace TapeDump
                     if (tape[p++] != 0xba) c = 0; else c++;
                     if (c == 3) break;
                 }
+                if (p == tape.Length)
+                {
+                    Console.Error.WriteLine("Skipped {0:D0} trailer bytes", p - q);
+                    break;
+                }
                 if (c == 3)
                 {
                     Console.Error.WriteLine("End marker found after {0:D0} bytes, ignoring {1:D0} following bytes", p - q - 3, tape.Length - p);
@@ -225,42 +230,63 @@ namespace TapeDump
 
         static public void DumpBASIC(Byte[] tape)
         {
-            // leader
-            Int32 p = 0;
-            while (tape[p] != 0xff) p++;
-            if (p != 0) Console.Error.WriteLine("Skipped {0:D0} leader bytes", p);
-
-            // header
-            p++; // skip FF byte
-            Int32 len = -1;
-            for (Int32 i = 0; i < 2; i++)
+            Int32 p = 0, q = 0;
+            while (q < tape.Length)
             {
-                len <<= 8;
-                len |= tape[p++];
+                // leader
+                Int32 c = 0;
+                while ((p < tape.Length) && (tape[p] != 0xff))
+                {
+                    if (tape[p++] != 0xba) c = 0; else c++;
+                    if (c == 3) break;
+                }
+                if (p == tape.Length)
+                {
+                    Console.Error.WriteLine("Skipped {0:D0} trailer bytes", p - q);
+                    break;
+                }
+                if (c == 3)
+                {
+                    Console.Error.WriteLine("End marker found after {0:D0} bytes, ignoring {1:D0} following bytes", p - q - 3, tape.Length - p);
+                    break;
+                }
+                if (p != q) Console.Error.WriteLine("Skipped {0:D0} leader bytes", p - q);
+
+                // header
+                p++; // skip FF byte
+                Int32 len = -1;
+                for (Int32 i = 0; i < 2; i++)
+                {
+                    len <<= 8;
+                    len |= tape[p++];
+                }
+                len = -len;
+
+                // program text
+                Console.Error.WriteLine("Reading {0:D0} words of program text...", len);
+                UInt16[] text = new UInt16[len];
+                Int32 sum = 0;
+                for (Int32 i = 0; i < len; i++)
+                {
+                    text[i] = (UInt16)(tape[p++] << 8);
+                    text[i] |= tape[p++];
+                    sum += text[i];
+                }
+
+                // checksum
+                UInt16 checksum = (UInt16)(tape[p++] << 8);
+                checksum |= tape[p++];
+                sum += checksum;
+                sum &= 0xffff;
+                Console.Error.Write("Checksum: ");
+                if (sum == 0) Console.Error.WriteLine("{0:x4} OK", checksum);
+                else Console.Error.WriteLine("{0:x4} ERROR (expected {1:x4})", (sum + checksum) & 0xffff, checksum);
+
+                DumpBASIC(text);
+                Console.Out.WriteLine();
+
+                q = p;
             }
-            len = -len;
-
-            // program text
-            Console.Error.WriteLine("Reading {0:D0} words of program text...", len);
-            UInt16[] text = new UInt16[len];
-            Int32 sum = 0;
-            for (Int32 i = 0; i < len; i++)
-            {
-                text[i] = (UInt16)(tape[p++] << 8);
-                text[i] |= tape[p++];
-                sum += text[i];
-            }
-
-            // checksum
-            UInt16 checksum = (UInt16)(tape[p++] << 8);
-            checksum |= tape[p++];
-            sum += checksum;
-            sum &= 0xffff;
-            Console.Error.Write("Checksum: ");
-            if (sum == 0) Console.Error.WriteLine("{0:x4} OK", checksum);
-            else Console.Error.WriteLine("{0:x4} ERROR (expected {1:x4})", (sum + checksum) & 0xffff, checksum);
-
-            DumpBASIC(text);
         }
 
         static public void DumpBASIC(UInt16[] text)
